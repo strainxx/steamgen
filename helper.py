0d240750eee30f8d5f1df72f5bd4d999d80b612d
strainxx <yarik.chotkii@gmail.com>
2023-08-29 11:35:52 +0300

    BIG UPDATE 😳

--- helper.py
+++ helper.py
@@ -1,4 +1,4 @@
-def generateweb(templateFile: str, urls: list):
+def generateweb(templateFile: str, urls: list, titles: list):
     """This function generates webpage"""
     with open(templateFile, "r") as temp:
         tempText = temp.read()
@@ -6,6 +6,10 @@ def generateweb(templateFile: str, urls: list):
         t = 1
         for i in urls:
             finalText = finalText.replace(f"GAME_{t}_LOCATION", i)
+            t += 1
+        t = 1
+        for i in titles:
+            finalText = finalText.replace(f"GAME_{t}_NAME", i)
             t += 1
         with open("generated.html", "w") as generated:
             generated.write(finalText)
--- main.py
+++ main.py
@@ -13,6 +13,8 @@ goodReviewElement = "game_review_summary positive"
 t1 = time.time()
 
 urls = []
+titles = []
+
 
 urlCount = 5
 
@@ -24,19 +26,23 @@ while gamesCount <= urlCount:
     url = 'https://store.steampowered.com/app/' + str(random.randint(1, 1300000)) + "/"  # random url
     print("Sending request to", url)
     r = requests.get(url)
+    rtext = r.text
     # print(r.text)
-    isGame = isGameElement in r.text
+    isGame = isGameElement in rtext
+    title = rtext[rtext.find('<title>') + 7 : rtext.find('</title>')]
+    print(title)
     if isGame:
-        isGood = goodReviewElement in r.text
+        isGood = goodReviewElement in rtext
         gamesCount += 1
         print(f"Game found: {url} | Good reviews: {isGood}")
         urls.append(url)
+        titles.append(title)
 
 print(f"Finished in {time.time() - t1} seconds")
 print("Generating webpage...")
 
 
-helper.generateweb("template.html", urls)
+helper.generateweb("template.html", urls, titles)
 
 
 print("Successfully generated webpage :)")
